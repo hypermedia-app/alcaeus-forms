c42794c596d4f790ae8b9adf6e9ec74ee91e4c7d
tpluscode <tpluscode@users.noreply.github.com>
2019-06-17 13:45:53 +0200

    test: 💍 operations getter of alcaeus-form

--- test/alcaeus-form.test.ts
+++ test/alcaeus-form.test.ts
@@ -3,19 +3,50 @@ import { html, fixture, expect } from '@open-wc/testing'
 import '../src/alcaeus-form'
 
 describe('<alcaeus-form>', () => {
-  it('converts operation to contract', async () => {
-    // given
-    const operation = {}
+  describe('operation', () => {
+    it('is converted operation to contract', async () => {
+      // given
+      const operation = {}
 
-    // then
-    const el = await fixture(
-      html`
-        <alcaeus-form .operation="${operation}"></alcaeus-form>
-      `,
-    )
-    await el.updateComplete
+      // then
+      const el = await fixture(
+        html`
+          <alcaeus-form .operation="${operation}"></alcaeus-form>
+        `,
+      )
+      await el.updateComplete
 
-    // then
-    expect(el.contract).to.be.ok
+      // then
+      expect(el.contract).to.be.ok
+    })
+
+    it('throws when is falsy', async () => {
+      // then
+      const el = await fixture(
+        html`
+          <alcaeus-form></alcaeus-form>
+        `,
+      )
+      await el.updateComplete
+
+      // then
+      expect(() => el.operation).to.throw
+    })
+
+    it('returns the original value', async () => {
+      // given
+      const operation = {}
+
+      // then
+      const el = await fixture(
+        html`
+          <alcaeus-form .operation="${operation}"></alcaeus-form>
+        `,
+      )
+      await el.updateComplete
+
+      // then
+      expect(el.operation).to.equal(operation)
+    })
   })
 })
