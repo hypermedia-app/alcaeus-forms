fe868a8cdd21e2160e6ef5c13e7b04a992a6b4d6
tpluscode <tpluscode@users.noreply.github.com>
2019-06-17 10:27:02 +0200

    style: update indentation

--- src/lib/operation-to-contract.ts
+++ src/lib/operation-to-contract.ts
@@ -1,25 +1,30 @@
-import { HydraResource, IDocumentedResource, ISupportedOperation, SupportedProperty } from 'alcaeus/types/Resources'
+import {
+  HydraResource,
+  IDocumentedResource,
+  ISupportedOperation,
+  SupportedProperty,
+} from 'alcaeus/types/Resources'
 import { FieldContract, FormContract } from '@lit-any/forms/lib/formContract'
 
 interface FieldContractExtension {
-    collection: HydraResource;
+  collection: HydraResource
 }
 
 function propertyToField(sp: SupportedProperty & any): FieldContract & FieldContractExtension {
-    return {
-        type: sp.property.range.id,
-        property: sp.property.id,
-        title: sp.title,
-        description: sp.description,
-        required: sp.required,
-        collection: sp['http://www.w3.org/ns/hydra/core#collection'],
-    }
+  return {
+    type: sp.property.range.id,
+    property: sp.property.id,
+    title: sp.title,
+    description: sp.description,
+    required: sp.required,
+    collection: sp['http://www.w3.org/ns/hydra/core#collection'],
+  }
 }
 
 export default (operation: ISupportedOperation & IDocumentedResource): FormContract => ({
-    title: operation.title,
-    description: operation.description,
-    fields: operation.expects ? operation.expects.supportedProperties.map(propertyToField) : [],
-    method: operation.method,
-    target: '',
+  title: operation.title,
+  description: operation.description,
+  fields: operation.expects ? operation.expects.supportedProperties.map(propertyToField) : [],
+  method: operation.method,
+  target: '',
 })
